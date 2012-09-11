1a9f1cdd4c52f15cda97c62d02944eeaee59a712
Adrian Seeley <abseeley@gmail.com>
2012-09-11 08:45:30 -0400

    (setup in progress)

---
+++ src/com/magneticbear/scala1/Home.java
@@ -0,0 +1,20 @@
+package com.magneticbear.scala1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Home extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_home, menu);
+        return true;
+    }
+}
