66596c12bea73a74498f77cca773dc87b8b2b1b5
Adrian Seeley <abseeley@gmail.com>
2012-09-12 11:24:44 -0400

    events feed pulls json then toasts it at you

--- src/com/magneticbear/scala1/Events.java
+++ src/com/magneticbear/scala1/Events.java
@@ -1,8 +1,25 @@
 package com.magneticbear.scala1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class Events extends Activity {
 
@@ -10,6 +27,39 @@ public class Events extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        
+        // Read event feed
+        String rawEventFeedJSON = readEventFeed();
+        
+        // Parse event feed
+		try 
+		{
+			JSONObject o = new JSONObject(rawEventFeedJSON);
+			Toast.makeText(getBaseContext(), "Length of JSON: " + o.length(), Toast.LENGTH_LONG).show();
+			Toast.makeText(getBaseContext(), o.names().toString(), Toast.LENGTH_LONG).show();
+			
+			// Loop through each key in the JSON
+			for(int iter = 0; iter < o.names().length(); iter++)
+			{
+				String name = o.names().getString(iter);
+				
+				Toast.makeText(getBaseContext(), o.getString(name), Toast.LENGTH_LONG).show();
+			}
+			
+			//JSONArray jsonArray = new JSONArray(rawEventFeedJSON);
+			
+			//Log.i(Events.class.getName(), "Number of entries " + jsonArray.length());
+			
+			//for (int i = 0; i < jsonArray.length(); i++) 
+			{
+			//	JSONObject jsonObject = jsonArray.getJSONObject(i);
+				//Log.i(Events.class.getName(), jsonObject.getString("text"));
+			}
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
     }
 
     @Override
@@ -17,4 +67,46 @@ public class Events extends Activity {
         getMenuInflater().inflate(R.menu.activity_events, menu);
         return true;
     }
+    
+    
+    public String readEventFeed() 
+    {
+        StringBuilder builder = new StringBuilder();
+        HttpClient    client  = new DefaultHttpClient();
+        HttpGet       httpGet = new HttpGet(getString(R.string.url_events_feed));
+        
+        try 
+        {
+			HttpResponse response   = client.execute(httpGet);
+			StatusLine   statusLine = response.getStatusLine();
+			int 		 statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) 
+			{
+				Toast.makeText(getBaseContext(), "HTTP 200 on events feed GET.", Toast.LENGTH_LONG).show();
+				
+				HttpEntity     entity  = response.getEntity();
+				InputStream    content = entity.getContent();
+				BufferedReader reader  = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) 
+				{
+					builder.append(line);
+				}
+			} 
+			else 
+			{
+				Log.e(Events.class.getName(), "Failed to download events feed json.");
+				Toast.makeText(getBaseContext(), "Failed to download events feed json.", Toast.LENGTH_LONG).show();
+			}
+        } 
+        catch (ClientProtocolException e) 
+        {
+          e.printStackTrace();
+        } 
+        catch (IOException e) 
+        {
+          e.printStackTrace();
+        }
+        return builder.toString();
+      }
 }
