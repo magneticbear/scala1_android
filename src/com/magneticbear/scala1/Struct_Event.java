bee3ca44f1628f92ea9884ec5f468e3ca2383227
Adrian Seeley <abseeley@gmail.com>
2012-09-12 12:44:32 -0400

    building relevant event structure from JSON for adapter

--- src/com/magneticbear/scala1/Struct_Event.java
+++ src/com/magneticbear/scala1/Struct_Event.java
@@ -1,5 +1,17 @@
 package com.magneticbear.scala1;
 
-public class Struct_Event {
-
+public class Struct_Event 
+{
+	public String title;
+	public String start;
+	public String location;
+	public String eventid;
+	
+	public Struct_Event(String Title, String Start, String Location, String EventID)
+	{
+		title    = Title;
+		start    = Start;
+		location = Location;
+		eventid  = EventID;
+	}
 }
