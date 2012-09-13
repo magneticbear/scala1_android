b6ebf66f23e7cafd40ba586f975bd05cdab5ac89
Adrian Seeley <abseeley@gmail.com>
2012-09-13 08:56:04 -0400

    fancy date reader for dTtZ datetime strings

--- src/com/magneticbear/scala1/Struct_Event.java
+++ src/com/magneticbear/scala1/Struct_Event.java
@@ -1,17 +1,50 @@
 package com.magneticbear.scala1;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.text.format.Time;
+
 public class Struct_Event 
 {
 	public String title;
-	public String start;
+	public Date   start_date;
 	public String location;
 	public String eventid;
 	
 	public Struct_Event(String Title, String Start, String Location, String EventID)
 	{
-		title    = Title;
-		start    = Start;
-		location = Location;
-		eventid  = EventID;
+		title      = Title;
+		location   = Location;
+		eventid    = EventID;
+		start_date = readDate(Start);
+	}
+	
+	private Date readDate(String DateString)
+	{
+		// pull date and time from start string
+		//                      9   10   11      16    19     23
+		// SAMPLE::    2012-10-02    T    10:00   :00   .000   Z
+		//
+		// FIRST 10 CHARS ARE SIMPLE DATE
+		String datestring = DateString.substring(0, 10);
+		
+		// CHARS 11->16 ARE TIME HH:MM
+		String timestring =  DateString.substring(11, 16); 
+		 
+		// Format date
+		Date date = null;
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd:kk:mm"); 
+		try 
+		{
+			date = simpleDateFormat.parse(datestring + ":" + timestring);
+		} 
+		catch (ParseException e) 
+		{
+			e.printStackTrace();
+		} 
+		
+		return date;
 	}
 }
--- src/com/magneticbear/scala1/Struct_Event_Adapter.java
+++ src/com/magneticbear/scala1/Struct_Event_Adapter.java
@@ -1,9 +1,11 @@
 package com.magneticbear.scala1;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import android.content.Context;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +14,29 @@ import android.widget.TextView;
 
 public class Struct_Event_Adapter extends ArrayAdapter<Struct_Event> 
 {
+	private static final Boolean FLAG_EVENT_ITEM     = true;
+	private static final Boolean FLAG_SEPERATOR_ITEM = false;
+	
+	private ArrayList<Boolean>		flags;
 	private ArrayList<Struct_Event> items;
 
     public Struct_Event_Adapter(Context context, int textViewResourceId, ArrayList<Struct_Event> items) 
     {
 	    super(context, textViewResourceId, items);
+	    
+	    // Associate arraylist of struct_event items
 	    this.items = items;
+	    
+	    // Sort And Create Separators
+	    sortAndCreateSeparators();
     }
+    
+    private void sortAndCreateSeparators()
+    {
+    	
+    }
+    
+    
     @Override
     public View getView(int position, View convertView, ViewGroup parent) 
     {
