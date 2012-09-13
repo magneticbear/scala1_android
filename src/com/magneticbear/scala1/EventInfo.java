ad97e1eff88e3cfc70c506ba1cc55b8b9db1a6c3
Adrian Seeley <abseeley@gmail.com>
2012-09-13 12:40:56 -0400

    unified title styles, setup event info activity web stuff

--- src/com/magneticbear/scala1/EventInfo.java
+++ src/com/magneticbear/scala1/EventInfo.java
@@ -3,6 +3,8 @@ package com.magneticbear.scala1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class EventInfo extends Activity {
 
@@ -10,6 +12,19 @@ public class EventInfo extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
+        
+        // Setup debug data
+        String event_title_to_display = "PASS TITLE HERE";
+        int event_id_to_load = 1;
+        
+        // Navigate to event url
+        WebView event_info_webview = (WebView)findViewById(R.id.webview_event_info);
+        event_info_webview.getSettings().setJavaScriptEnabled(true);
+        event_info_webview.loadUrl(getString(R.string.url_event_info_base) + event_id_to_load);
+        
+        // Set title to event title
+        TextView title = (TextView)findViewById(R.id.event_info_title);
+        title.setText(event_title_to_display);
     }
 
     @Override
