d5beafdc77b08c1472d9a23bc7617f762dc26542
Adrian Seeley <abseeley@gmail.com>
2012-09-20 16:19:12 -0400

    splash now pulls user data on load, shows long splash if it hasnt been seen before

--- src/com/magneticbear/scala1/UserData.java
+++ src/com/magneticbear/scala1/UserData.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class UserData 
 {
-	public static ArrayList<Struct_Event>   fav_events   = null;
-	public static ArrayList<Struct_Speaker> fav_speakers = null;
+	public static Boolean 					has_seen_splash = false;
+	public static ArrayList<Struct_Event>   fav_events   	= null;
+	public static ArrayList<Struct_Speaker> fav_speakers 	= null;
 	
 	public static void load_or_create()
 	{
 		if(fav_events == null || fav_speakers == null)
 		{
 			// do load here gee
-		
 			fav_events   = new ArrayList<Struct_Event>();
 			fav_speakers = new ArrayList<Struct_Speaker>();
 		}
