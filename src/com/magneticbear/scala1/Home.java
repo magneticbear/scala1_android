a4babd46999ac8b6e0407746980d74578f149d70
Adrian Seeley <abseeley@gmail.com>
2012-09-20 12:26:51 -0400

    home buttons now work (back supported)

--- src/com/magneticbear/scala1/Home.java
+++ src/com/magneticbear/scala1/Home.java
@@ -2,7 +2,11 @@ package com.magneticbear.scala1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Home extends Activity {
 
@@ -10,11 +14,63 @@ public class Home extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        setupButtons();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
+    }
+    
+    public void setupButtons()
+    {
+    	findViewById(R.id.btn_events).setOnClickListener(new OnClickListener() 
+    	{
+			@Override
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(getBaseContext(), Events.class);
+				startActivity(intent);
+			}
+		});
+    	findViewById(R.id.btn_speakers).setOnClickListener(new OnClickListener() 
+    	{
+			@Override
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(getBaseContext(), Speakers.class);
+				startActivity(intent);
+			}
+		});
+    	findViewById(R.id.btn_favourites).setOnClickListener(new OnClickListener() 
+    	{
+			@Override
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(getBaseContext(), Favourites.class);
+				startActivity(intent);
+			}
+		});
+    			
+    	findViewById(R.id.btn_playground).setOnClickListener(new OnClickListener() 
+    	{
+			@Override
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(getBaseContext(), Playground.class);
+				startActivity(intent);
+			}
+		});
+    	findViewById(R.id.btn_about).setOnClickListener(new OnClickListener() 
+    	{
+			@Override
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(getBaseContext(), About.class);
+				startActivity(intent);
+			}
+		});
     }
 }
