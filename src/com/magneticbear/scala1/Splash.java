41c28ffb42513bd43b759642e6e36ee87ce08238
Adrian Seeley <abseeley@gmail.com>
2012-09-20 12:08:53 -0400

    hold splash for 2sec then go to home

--- src/com/magneticbear/scala1/Splash.java
+++ src/com/magneticbear/scala1/Splash.java
@@ -1,7 +1,11 @@
 package com.magneticbear.scala1;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class Splash extends Activity {
@@ -12,6 +16,18 @@ public class Splash extends Activity {
         setContentView(R.layout.activity_splash);
         
         
+        Timer splashtimer = new Timer();
+        splashtimer.schedule(new TimerTask() 
+        {
+			@Override
+			public void run()
+			{
+				Intent intent = new Intent(getBaseContext(), Home.class);
+				startActivity(intent);
+		        finish();
+			}
+		}, 2000);
+
     }
 
     @Override
